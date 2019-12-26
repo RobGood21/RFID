6e11678032ed883056007995d6c0abd02d214478
Rob Antonisse <robantonisse@gmail.com>
2019-12-26 11:24:49 +0100

    RFID

---
+++ WriteUID/WriteUID/WriteUID.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WriteUID.ino
+ Created:	12/26/2019 11:23:55 AM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
