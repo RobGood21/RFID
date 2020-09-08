be55e1f145120878d9825f1ce0dbd45e47877cad
Rob Antonisse <robantonisse@gmail.com>
2020-09-08 21:53:55 +0200

    Dices

---
+++ Dices/Dices.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Dices.ino
+ Created:	9/8/2020 9:53:29 PM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
